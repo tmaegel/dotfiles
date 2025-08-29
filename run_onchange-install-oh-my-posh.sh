2d4558f25bbaa751bd2628725aa1e1f5ce7094cb
Toni Mägel <tmaegel@posteo.de>
2025-08-29 20:47:36 +0200

    feat: Add oh-my-posh install script

---
+++ run_onchange-install-oh-my-posh.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# -u: Treat unset variables as an error when substituting.
+# -e: Exit immediately if a command exits with a non-zero status.
+# -o pipefail: the return value of a pipeline is the status of the
+#              last command to exit with a non-zero status.
+set -eo pipefail
+
+VERSION="v26.19.2"
+
+wget --quiet \
+    "https://github.com/JanDeDobbeleer/oh-my-posh/releases/download/${VERSION}/posh-linux-amd64"
+    -O ~/.local/bin/oh-my-posh.new
+cp ~/.local/bin/oh-my-posh.new ~/.local/bin/oh-my-posh
+chmod 544 ~/.local/bin/oh-my-posh
+rm ~/.local/bin/oh-my-posh.new
